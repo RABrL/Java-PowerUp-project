b707c06c88fcbd11e3ac87b58addef32b2f1ae21
RABrL <robin_brito@hotmail.com>
2023-03-05 20:50:03 -0500

    Add new controllers exceptions in ControllerAdvisor class

--- src/main/java/com/example/restaurant_plaza/infrastructure/exceptionhandler/ControllerAdvisor.java
+++ src/main/java/com/example/restaurant_plaza/infrastructure/exceptionhandler/ControllerAdvisor.java
@@ -1,6 +1,8 @@
 package com.example.restaurant_plaza.infrastructure.exceptionhandler;
 
 import com.example.restaurant_plaza.infrastructure.exception.NoDataFoundException;
+import com.example.restaurant_plaza.infrastructure.exception.UserAlreadyExistException;
+import com.example.restaurant_plaza.infrastructure.exception.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -18,5 +20,17 @@ public class ControllerAdvisor {
     public ResponseEntity<Map<String, String>> handleNoDataFoundException(NoDataFoundException exception) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(Collections.singletonMap(MESSAGE,ExceptionResponse.NO_DATA_FOUND.getMessage()));
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleUserNotFoundException(UserNotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(Collections.singletonMap(MESSAGE,ExceptionResponse.USER_NOT_FOUND.getMessage()));
+    }
+
+    @ExceptionHandler(UserAlreadyExistException.class)
+    public ResponseEntity<Map<String, String>> handleUserAlreadyExistException(UserAlreadyExistException exception) {
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(Collections.singletonMap(MESSAGE,ExceptionResponse.USER_ALREADY_EXIST.getMessage()));
     }
 }
--- src/main/java/com/example/restaurant_plaza/infrastructure/exceptionhandler/ExceptionResponse.java
+++ src/main/java/com/example/restaurant_plaza/infrastructure/exceptionhandler/ExceptionResponse.java
@@ -1,7 +1,9 @@
 package com.example.restaurant_plaza.infrastructure.exceptionhandler;
 
 public enum ExceptionResponse {
-    NO_DATA_FOUND("No data found for the request petition");
+    NO_DATA_FOUND("No data found for the request petition"),
+    USER_NOT_FOUND("User not exist in the database"),
+    USER_ALREADY_EXIST("User already exist in the database");
 
     private final String message;
 
