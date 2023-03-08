14c7ae2fcd9fd5f493a5202152b2bf28c89c99c4
RABrL <robin_brito@hotmail.com>
2023-03-08 01:54:12 -0500

    Fix sintaxis typo

--- src/main/java/com/example/restaurant_plaza/infrastructure/exceptionhandler/ControllerAdvisor.java
+++ src/main/java/com/example/restaurant_plaza/infrastructure/exceptionhandler/ControllerAdvisor.java
@@ -23,7 +23,7 @@ public class ControllerAdvisor {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String,String>> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
+        ex.getBindingResult().getAllErrors().forEach(error -> {
             String fieldName = ((FieldError) error).getField();
             String errorMessage = error.getDefaultMessage();
             errors.put(fieldName, errorMessage);
