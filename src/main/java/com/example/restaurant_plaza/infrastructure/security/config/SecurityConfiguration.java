0bcfa0ba04c87970452422146d50b6c4c653b002
RABrL <robin_brito@hotmail.com>
2023-11-18 15:10:18 -0500

    Fix indentation in build.gradle and remove unused import in SecurityConfiguration.java

--- src/main/java/com/example/restaurant_plaza/infrastructure/security/config/SecurityConfiguration.java
+++ src/main/java/com/example/restaurant_plaza/infrastructure/security/config/SecurityConfiguration.java
@@ -1,6 +1,5 @@
 package com.example.restaurant_plaza.infrastructure.security.config;
 
-import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
